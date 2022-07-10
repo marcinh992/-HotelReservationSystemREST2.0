ac48064696ab8d32aa485857e0e7b523733efa74
Marcin Pypec <marcin.pypec@investimetric.io>
2022-07-10 14:54:05 +0200

    Initial commit

---
+++ src/main/java/com/reservationapp/demo/ReservationsystemServiceApplication.java
@@ -0,0 +1,13 @@
+package com.reservationapp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationsystemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationsystemServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reservationapp/demo/ReservationsystemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservationapp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationsystemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
